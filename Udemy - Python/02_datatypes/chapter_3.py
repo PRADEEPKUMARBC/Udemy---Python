3d39bebc3bb45d4af656f1a77e6a7a3e794f2cbb
Error . . . <pradeepkumarbc138@gmail.com>
2026-09-19 17:21:13 +0530

    Add files via upload

---
+++ Udemy - Python/02_datatypes/chapter_3.py
@@ -0,0 +1,20 @@
+# Integer
+
+black_tea_grams = 40
+ginger_grams = 5
+
+total_grams = black_tea_grams + ginger_grams
+print(f"Total grams of base tea: {total_grams}")
+
+remainig_grams = black_tea_grams - ginger_grams
+print(f"Remaining grams of base tea: {remainig_grams}")
+
+milk_liters = 7
+serving = 2
+milk_per_serving = milk_liters / serving
+print(f"Milk per serving: {milk_per_serving} liters")
+
+total_tea_bags = 100
+pots = 4
+bags_per_pot = total_tea_bags // pots
+print(f"Bags of tea per pot: {bags_per_pot}")
