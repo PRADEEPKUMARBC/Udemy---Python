b820f89e05ef772b33e830e3a08469a20880bf8e
Error . . . <pradeepkumarbc138@gmail.com>
2026-09-20 10:47:56 +0530

    Add files via upload

---
+++ Udemy - Python/02_datatypes/chapter_8.py
@@ -0,0 +1,37 @@
+# ingradients = ["ginger", "cardmom", "cinnamon", "cloves", "black pepper"]
+# ingradients.append("fennel")
+# print(f"Masala ingradients: {ingradients}")
+# ingradients.remove("ginger")
+# print(f"Masala ingradients after removing ginger: {ingradients}")
+
+# spice_options = ["cardmom", "cinnamon"]
+# chai_ingredients = ["ginger", "cloves", "black pepper"]
+# chai_ingredients.extend(spice_options)
+# print(f"Chai ingradients: {chai_ingredients}")
+# chai_ingredients.insert(2, "black tea")
+# print(f"Chai ingradients after adding black tea: {chai_ingredients}")
+
+# last_added = chai_ingredients.pop()
+# print(f"Last added ingradient: {last_added}")
+
+# print(f"Chai ingradients after removing last added: {chai_ingredients}")
+# chai_ingredients.sort()
+# print(f"Sorted chai ingradients: {chai_ingredients}")
+# chai_ingredients.reverse()
+# print(f"Reversed chai ingradients: {chai_ingredients}")
+
+sugar_level = [1, 2, 3, 4, 5]
+print(f"Maximum sugar level: {max(sugar_level)}")
+print(f"Minimum Sugar Level: {min(sugar_level)}")
+
+base_liquid = ["water", "milk"]
+extra_flavour = ["vanilla", "chocolate", "caramel"]
+
+full_mixed = base_liquid + extra_flavour
+print(f"Full mixed ingradients: {full_mixed}")
+
+strong_brew = ["black tea", "ginger", "cardmom", "cinnamon", "cloves", "black pepper"]
+print(f"Strong brew ingradients: {strong_brew}")
+
+raw_spice_data = bytearray(b"CINNAMON")
+print(f"Bytes : {raw_spice_data}")
