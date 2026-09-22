593747b290002bd6772c97bed16b146e21c631f0
Error . . . <pradeepkumarbc138@gmail.com>
2026-09-22 22:49:33 +0530

    Add files via upload

---
+++ Udemy - Python/03_conditionals/chai_price_calculator.py
@@ -0,0 +1,10 @@
+cup = input("Enter the cup size (small/medium/large) : ").lower()
+
+if cup == "small":
+    print("Price is 10 rs ")
+elif cup == "Medium":
+    print("Price is 15 rs")
+elif cup == "Large":
+    print("Price is 30 rs")
+else:
+    print("Invalid")
---
+++ Udemy - Python/03_conditionals/snack_suggestion.py
@@ -0,0 +1,6 @@
+snack = input("Enter the snack : ").lower()
+
+if snack == "Cookies" or snack == "Burger":
+    print(f" Great Choice we'll serve you {snack}")
+else:
+    print(f" Sorry we only serve cookies or samosa with tea")
