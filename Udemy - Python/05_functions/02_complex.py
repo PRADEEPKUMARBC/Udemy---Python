a78ad707a3e3034fca6f27c9dae03bed3f862283
Error . . . <pradeepkumarbc138@gmail.com>
2026-09-26 14:31:57 +0530

    Add files via upload

---
+++ Udemy - Python/05_functions/02_complex.py
@@ -0,0 +1,14 @@
+def Namaskara(name, opposite_name):
+    print(f" {name} namaskara pa bolimagana {opposite_name}")
+
+
+def Hegidira(nivu, navu):
+    print(f" Arama adiya {nivu}")
+
+
+def banda_pa_bosudimaga(nayi):
+    Namaskara("Pradeep", "Pradee")
+    Hegidira("Pradee", "nanu")
+    print("Hattra Ninga")
+
+banda_pa_bosudimaga("Praep")
